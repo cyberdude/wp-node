dac0525d752d21414d3ecd3d92a24dcce48652e6
Arnaldo Capo <acapo@directv.com>
2013-08-26 19:57:20 -0400

    Added now there's a time management and a better mongo cache object with the TTL

--- index.js
+++ index.js
@@ -4,38 +4,94 @@ var request = require('request')
 
 function wpnode(options, fn) {
 
+	this.TTL = 30;
+
 	var url = options.url
 		, db = options.db;
 
-	if (ourUrls.length === 0) {
 
-		request.get({
-			url: url
-		}, function(e, r, b){
+		//Go Straight to mongo
+		db.collection('cache', function(err, collection) {
+			collection.findOne({id: url}, function(err, item) {
 			
-			b = JSON.parse(b);
+				if (!item) {
+					console.log('Getting cache for' + url);
+					// request start
+					request.get({
+						url: url
+					}, function(e, r, b){
+
+						b = JSON.parse(b);
+
+						var cache_object = {
+							id: url, 
+							content: b,
+							wp_timestamp: +new Date()
+						}
+
+						db.collection('cache', function(err, collection) {
+			        collection.insert(cache_object, {safe:true}, function(err, result) {
+			            if (err) {
+			            	return err;
+			            } else {
+			                console.log('Success: ' + JSON.stringify(result));
+			                fn(result[0].content);
+			            }
+			    		});
+		    		});
+					}); //request end
+				} else {
+
+					//Check if we are over the cache limit
+					console.log(item.wp_timestamp);
+
+					var currentTime = +new Date();
+
+					if ( ((currentTime - item.wp_timestamp)/1000) > this.TTL) {
+						console.log('Getting fresh content for ' + url);
+
+
+					}
+
+
+
+					fn(item.content);
+
+				}
+			});
+		});
+/*
+		
+
+			
 
 			db.collection('cache', function(err, collection) {
-		        collection.insert(b, {safe:true}, function(err, result) {
+		        collection.insert(cache_object, {safe:true}, function(err, result) {
 		            if (err) {
 		            	return err;
 		            } else {
 		                console.log('Success: ' + JSON.stringify(result));
-		                fn(result);
+		                fn(result[0].content);
 		            }
 		    	});
 	    	});
 			
 		});
 		
-	}
-	// for (var x = 0; x < ourUrls.length; x++) {
-	// 	if (ourUrls[x] == url) {
-	// 		console.log('it is in cache');
-	// 		break;
-	// 	}	
-	// }
-
+*/
 }
 
+wpnode.clearBot = function(url, db) {
+	console.log('Clearing Cache');
+	db.collection('cache', function(err, collection) {
+        collection.findOne({id: url}, function(err, item) {
+            if (err)
+            	console.log(err);
+
+            console.log('Removing cache for ' + url);
+
+
+        });
+    });
+}
 module.exports = wpnode;
