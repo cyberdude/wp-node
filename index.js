ff66052739a8c801bba2c554bef49409eac2fd00
Arnaldo Capo <acapo@directv.com>
2013-08-26 14:49:06 -0400

    Now ignoring node_modules and initial structure of wpnode

--- index.js
+++ index.js
@@ -1,3 +1,41 @@
-module.exports = function(url) {
-	return url;
+
+var request = require('request')
+	, ourUrls = [];
+
+function wpnode(options, fn) {
+
+	var url = options.url
+		, db = options.db;
+
+	if (ourUrls.length === 0) {
+
+		request.get({
+			url: url
+		}, function(e, r, b){
+			
+			b = JSON.parse(b);
+
+			db.collection('cache', function(err, collection) {
+		        collection.insert(b, {safe:true}, function(err, result) {
+		            if (err) {
+		            	return err;
+		            } else {
+		                console.log('Success: ' + JSON.stringify(result));
+		                fn(result);
+		            }
+		    	});
+	    	});
+			
+		});
+		
+	}
+	// for (var x = 0; x < ourUrls.length; x++) {
+	// 	if (ourUrls[x] == url) {
+	// 		console.log('it is in cache');
+	// 		break;
+	// 	}	
+	// }
+
 }
+
+module.exports = wpnode;
